c8044f6be3d6c1a8c582f4a10765136c89b1c73a
1660136 <1660136@student.hcmus.edu.vn>
2018-11-21 01:47:33 +0700

    Update

--- reg.php
+++ reg.php
@@ -35,7 +35,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>login</title>
+	<title>Register</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="assets/bootstrap-3.3.7-dist/css/bootstrap.min.css">
 </head>
